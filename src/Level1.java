93a34ca4d15d1c9c5c766ad38561c58b5c8784d0
3uomlkh <cw1205@duksung.ac.kr>
2024-12-16 15:23:22 +0900

    refactor: Lv.1 랜덤 닉네임 생성기(메서드 분리)

--- src/Level1.java
+++ src/Level1.java
@@ -1,17 +1,22 @@
 import java.util.Random;
 
+// Lv1. 랜덤 닉네임 생성기
 public class Level1 {
-    public static void main(String[] args) {
-        // Lv1. 랜덤 닉네임 생성기
-        String[] keyword1 = {"기철초풍", "멋있는", "재미있는"};
-        String[] keyword2 = {"도전적인", "노란색의", "바보같은"};
-        String[] keyword3 = {"돌고래", "개발자", "오랑우탄"};
+    private String[] keyword1 = {"기철초풍", "멋있는", "재미있는"};
+    private String[] keyword2 = {"도전적인", "노란색의", "바보같은"};
+    private String[] keyword3 = {"돌고래", "개발자", "오랑우탄"};
 
+    public String createRandomNickname() {
         Random random = new Random();
         int i1 = random.nextInt(keyword1.length); // 0 ~ 2 사이의 랜덤 인덱스
         int i2 = random.nextInt(keyword1.length);
         int i3 = random.nextInt(keyword1.length);
+        return keyword1[i1] + " " + keyword2[i2] + " " + keyword3[i3];
+    }
 
-        System.out.print("랜덤 닉네임: " + keyword1[i1] + " " + keyword2[i2] + " " + keyword3[i3]);
+    public static void main(String[] args) {
+        Level1 level1 = new Level1();
+        String randomNickname = level1.createRandomNickname();
+        System.out.print("랜덤 닉네임: " + randomNickname);
     }
 }
